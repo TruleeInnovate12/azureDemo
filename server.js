a327a28c0b578c320c111722ecbb4c00bc770315
MansoorShaik12 <shaikmansoor1200@gmail.com>
2024-11-24 23:42:07 +0530

    trying to call frontend from backend

--- server.js
+++ server.js
@@ -1,43 +1,17 @@
-// const express = require('express');
-// const cors = require('cors');
-
-// const app = express();
-// const port = process.env.PORT || 4040;
-
-// app.use(cors());
-
-// app.get('/', (req, res) => {
-//     res.send('Hello!');
-// });
-
-// app.get('/api/message', (req, res) => {
-//     res.json({ message: 'Hello from the backend!' });
-// });
-
-// app.listen(port, () => {
-//     console.log(`Server is running on http://localhost:${port}`);
-// });
-
-
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 4040;
 
 app.use(cors());
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.get('/', (req, res) => {
+    res.send('Hello!');
+});
 
 app.get('/api/message', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
-});
-
-// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 
 app.listen(port, () => {
