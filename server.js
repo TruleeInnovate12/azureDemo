b37c6b29fd1723f7a85db9ae7f69f6b65972e4e4
MansoorShaik12 <shaikmansoor1200@gmail.com>
2024-11-25 19:11:11 +0530

    corrected http to https

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');
 
 const app = express();
 const port = process.env.PORT || 4040;
 
 app.use(cors());
-app.use(helmet());
 
 app.get('/', (req, res) => {
     res.send('Hello!');
@@ -14,12 +12,6 @@ app.get('/', (req, res) => {
 
 app.get('/api/message', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
 });
 
 app.listen(port, () => {
