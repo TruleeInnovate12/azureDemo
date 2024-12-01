b1fd7777f08320083d846e6fba748ffe82c57f93
Ashraf-03 <ashrafshaik250@gmail.com>
2024-12-01 21:04:57 +0530

    ashraf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
+    // ...existing config
     rules: {
-      'no-unused-vars': 'warn', // Change from 'error' to 'warn'
-      'react/jsx-pascal-case': 'warn',
-      'react-hooks/exhaustive-deps': 'warn'
-    }
-  };
+      'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+      'no-useless-escape': 'warn',
+      'jsx-a11y/heading-has-content': 'warn',
+      'react/jsx-pascal-case': 'warn'
+    },
+    // ...rest of config
+  }
