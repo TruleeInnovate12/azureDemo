f0d96b8238f98248847d32ce7220e4938f0993b0
Ashraf-03 <ashrafshaik250@gmail.com>
2024-12-01 19:22:16 +0530

    ashraf

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    rules: {
+      'no-unused-vars': 'warn', // Change from 'error' to 'warn'
+      'react/jsx-pascal-case': 'warn',
+      'react-hooks/exhaustive-deps': 'warn'
+    }
+  };
